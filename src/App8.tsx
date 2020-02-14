3f2d552cfd30461b5736c56c8940d9f9d55d7232
Arcot Nishanth <nsharc4@gmail.com>
2020-02-14 22:39:56 +0530

    add lifeCycleMethods

---
+++ src/App8.tsx
@@ -0,0 +1,36 @@
+import React from 'react'
+
+interface MyProps {
+
+}
+
+interface MyState {
+
+}
+
+
+class App8 extends React.Component<MyProps, MyState> {
+  constructor() {
+    super();
+    this.state = {
+
+    }
+  }
+  componentDidMount() {
+
+  }
+
+  componentWillReceiveProps() {
+
+  }
+
+  render() {
+    return (
+      <div>
+
+      </div>
+    )
+  }
+}
+
+export default App8
